d54d9b7e1fb01cd9956d37d7a69caeeea0af1f23
Gautier B <bonhur.gautier@gmail.com>
2023-04-04 23:41:33 +0200

    [ADD] change graphical lib in menu

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -51,8 +51,17 @@ int main(int argc, char **argv)
 
     std::unique_ptr<Display::IWindow> argv_one_display_module = create_display_module(argv_one_library_dynamic);
     std::unique_ptr<Display::IWindow> ncurses_display_module = create_display_module(ncurses_library_dynamic);
+    std::unique_ptr<Display::IWindow> sdl2_display_module = create_display_module(sdl2_library_dynamic);
+    std::unique_ptr<Display::IWindow> sfml_display_module = create_display_module(sfml_library_dynamic);
 
     std::vector<std::string> games = {"lib/arcade_snake.so", "lib/arcade_nibbler.so"};
+    std::vector<std::unique_ptr<Display::IWindow>> display_module_vector_menu;
+
+    display_module_vector_menu.push_back(std::move(argv_one_display_module));
+    display_module_vector_menu.push_back(std::move(ncurses_display_module));
+    display_module_vector_menu.push_back(std::move(sfml_display_module));
+    display_module_vector_menu.push_back(std::move(sdl2_display_module));
+
 
     typedef std::unique_ptr<Display::IClock> (*CreateClockModuleFunction)();
     CreateClockModuleFunction createClockModule =
@@ -63,28 +72,29 @@ int main(int argc, char **argv)
 
     std::unique_ptr<Display::IClock> clockModule = createClockModule();
 
+    int idx_menu_graph = 0;
     bool isRunning = true;
     int startX = 0;
     size_t gameIndex = 0;
 
-    argv_one_display_module->create("Menu", 60, 1920, 1080);
+    display_module_vector_menu[idx_menu_graph]->create("Menu", 60, 1920, 1080);
     while (isRunning) {
         if (clockModule != nullptr && clockModule->getElapsedTime() > 1000) {
-            argv_one_display_module->clear();
+            display_module_vector_menu[idx_menu_graph]->clear();
             for (size_t i = 0; i < games.size(); ++i) {
                 startX = 5;
                 for (size_t j = 0; j < games[i].length(); ++j)
-                    argv_one_display_module->drawCharacter(startX + j, i, games[i][j]);
+                    display_module_vector_menu[idx_menu_graph]->drawCharacter(startX + j, i, games[i][j]);
                 if (i == gameIndex) {
-                    argv_one_display_module->drawCharacter(startX - 2, i, '-');
-                    argv_one_display_module->drawCharacter(startX - 2, i, '>');
+                    display_module_vector_menu[idx_menu_graph]->drawCharacter(startX - 2, i, '-');
+                    display_module_vector_menu[idx_menu_graph]->drawCharacter(startX - 2, i, '>');
                 }
             }
-            argv_one_display_module->display();
+            display_module_vector_menu[idx_menu_graph]->display();
             clockModule->restart();
         }
 
-        Display::KeyType event = argv_one_display_module->getEvent();
+        Display::KeyType event = display_module_vector_menu[idx_menu_graph]->getEvent();
 
         if (event == Display::KeyType::Z) {
             if (gameIndex > 0) {
@@ -94,6 +104,15 @@ int main(int argc, char **argv)
             if (gameIndex < games.size() - 1) {
                 ++gameIndex;
             }
+        } else if (event == Display::KeyType::P) {
+            if (idx_menu_graph + 1 >= static_cast<int>(display_module_vector_menu.size())) {
+                display_module_vector_menu[idx_menu_graph]->close();
+                idx_menu_graph = 0;
+            } else {
+                display_module_vector_menu[idx_menu_graph]->close();
+                idx_menu_graph++;
+            }
+            display_module_vector_menu[idx_menu_graph]->create("Menu", 60, 1920, 1080);
         } else if (event == Display::KeyType::E) {
             DynamicLibraryHandler gameLibraryHandler;
 
@@ -123,7 +142,7 @@ int main(int argc, char **argv)
 
             gameModule->init(new_display_module_vector);
 
-            argv_one_display_module->create("Menu", 60, 1920, 1080);
+            display_module_vector_menu[idx_menu_graph]->create("Menu", 60, 1920, 1080);
         } else if (event == Display::KeyType::X) {
             isRunning = false;
         }
